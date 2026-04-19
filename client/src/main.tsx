6d62d37eead784544d90aacfe306cb7b5b6f2904
mshary-web <mshary.3bdelr7man@gmail.com>
2026-04-19 16:06:03 +0300

    Remove BrowserRouter from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/Student_life_stimulator">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
